6a9b30f11f528ec01fab9140fe277f0fa7804af4
GarrySun9993 <646897519@qq.com>
2021-03-24 22:48:24 +0800

    添加项目文件。

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        private LogAnalyzer m_analyzer = null;
+
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            m_analyzer = new LogAnalyzer();
+        }
+
+        [TestCase("filewithbadextension.foo", false)]
+        [TestCase("filewithgoodextension.SLF", true)]
+        [TestCase("filewithgoodextension.slf", true)]
+        public void IsValidLogFileName_VariousExtension_CheckThem(string file, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("badfile.foo", false)]
+        [TestCase("goodfile.slf", true)]
+        public void IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string file, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            analyzer.IsValidLogFileName(file);
+            Assert.AreEqual(expected, analyzer.WasLastFileNameValid);
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_ThrowsArgumentException()
+        {
+            LogAnalyzer analyzer = MakeAnalyzer();
+            var ex = Assert.Catch<ArgumentException>(() => analyzer.IsValidLogFileName(string.Empty));
+            StringAssert.Contains("filename has to be provided", ex.Message);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            m_analyzer = null;
+        }
+    }
+}
---
+++ LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogAn.UnitTests
+{
+    public class MemCalculatorTests
+    {
+        private static MemCalculator MakeCalculator()
+        {
+            return new MemCalculator();
+        }
+
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calculator = MakeCalculator();
+            int lastSum = calculator.Sum();
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calculator = MakeCalculator();
+            calculator.Add(1);
+            int sum = calculator.Sum();
+            Assert.AreEqual(1, sum);
+        }
+    }
+}
